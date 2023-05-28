fa466bdedf5194f9836df84a26eedcab6d171d31
Akashi <akashi_sai@163.com>
2023-05-28 17:02:26 +0800

    :tada: feat: add api router

---
+++ api/nogizaka/members.ts
@@ -0,0 +1,6 @@
+import jsonData from '../../data/no-member.json'
+
+export default eventHandler(async (event) => {
+
+  return jsonData
+})
--- routes/index.ts
+++ routes/index.ts
@@ -1,3 +1,26 @@
 export default defineEventHandler(() => {
-  return { 'index': 'sakamichi' }
+  return `
+    <h2>Sakamichi Data Info API Serve</h2>
+    <ul>
+      <li><div>Nogizaka</div></li>
+      <ul>
+      <li><a href="/api/nogizaka/members">members</a></li>
+      </ul>
+
+      <li><div>Kiyakizaka</div></li>
+      <ul>
+      <li><a href="/api/kiyakizaka/members">members</a></li>
+      </ul>
+
+      <li><div>Hinatazaka</div></li>
+      <ul>
+      <li><a href="/api/hinatazaka/members">members</a></li>
+      </ul>
+
+      <li><div>Sakurazaka</div></li>
+      <ul>
+      <li><a href="/api/sakurazaka/members">members</a></li>
+      </ul>
+    </ul>
+  `
 })
