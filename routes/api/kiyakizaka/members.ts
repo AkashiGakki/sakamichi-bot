2dee6ffb68ba480409aff7131e7e076f3918da48
Akashi <akashi_sai@163.com>
2023-06-23 19:23:12 +0800

    :sparkles: feat: fetch kiyaki data

--- routes/api/kiyakizaka/members.ts
+++ routes/api/kiyakizaka/members.ts
@@ -1,12 +1,8 @@
+import { kMember } from '@/utils/kiyaki'
+
 export default eventHandler(async () => {
   const store = await useStorage('kiyaki')
   await store.setItem('techi', { techi: 'Hirate Yurina' })
 
-  const techi = await store.getItem('techi')
-  console.log('techi:', techi)
-
-  return {
-    'kiyakizaka': true,
-    techi
-  }
+  return kMember()
 })
--- utils/kiyaki.ts
+++ utils/kiyaki.ts
@@ -1,6 +1,43 @@
-export const cachedKiMemberInfo = cachedFunction(() => {
-  // fetch kiyaki data
-}, {
-  maxAge: 60 * 60 * 12 * 7,
-  name: 'KiMemberInfo'
-})
+import fs from 'node:fs'
+import { load } from 'cheerio'
+
+const url = `${process.cwd()}/data/saku/ja-saku.html`
+const ogUrl = `${process.cwd()}/data/saku/ja-saku-og.html`
+
+const table = fs.readFileSync(url)
+const ogTable = fs.readFileSync(ogUrl)
+
+export const kMember = () => {
+  const $ = load(table)
+  const items = $('tbody tr').toArray().map((member) => {
+    const content = load(member)
+
+    if (content('td:nth-child(7)').text() !== '1期生')
+      return
+
+    return {
+      name: content('td:nth-child(1)').text(),
+      kana: content('td:nth-child(2)').text(),
+      cate: content('td:nth-child(7)').text(),
+      birthday: content('td:nth-child(3)').text().split('_')[0],
+      blood: content('td:nth-child(5)').text(),
+      height: content('td:nth-child(6)').text(),
+    }
+  }).filter(Boolean)
+
+  const og = load(ogTable)
+  const ogItems = og('tbody tr').toArray().map((member) => {
+    const content  = load(member)
+
+    return {
+      name: content('td:nth-child(1)').text(),
+      kana: content('td:nth-child(2)').text(),
+      cate: content('td:nth-child(7)').text(),
+      birthday: content('td:nth-child(3)').text().split('_')[0],
+      blood: content('td:nth-child(5)').text().trim(),
+      height: content('td:nth-child(6)').text().trim(),
+    }
+  }).reverse()
+
+  return items.concat(ogItems)
+}
