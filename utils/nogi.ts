53b2a332aa90b32686dcd9d953766bfed17c86d2
Akashi <akashi_sai@163.com>
2023-06-24 16:42:39 +0800

    :zap: refactor: utils

--- utils/nogi.ts
+++ utils/nogi.ts
@@ -1,9 +1,7 @@
 import { $fetch } from 'ofetch'
 import { getObjectField } from '@use-kit/functions'
-import fs from 'node:fs'
-import { load } from 'cheerio'
 
-import { defaultFields } from '.'
+import { defaultFields } from '@/utils'
 import jsonData from '@/data/no-member.json'
 
 const dataCleaning = (str: string): Record<string, unknown> => {
@@ -21,60 +19,10 @@ export const fetchNoMembers = async () => {
   return []
 }
 
-const presetNoMembers = () => {
-  const members = jsonData.data
+export const presetNoMembers = (json: Record<string, unknown>[]) => {
+  const members = json
     .filter((m: Record<string, unknown>) => m.code !== '10001')
     .map((m: Record<string, unknown>) => getObjectField(m, defaultFields))
 
   return members
 }
-
-const url = `${process.cwd()}/data/nogi/ja-nogi.html`
-const ogUrl = `${process.cwd()}/data/nogi/ja-nogi-og.html`
-
-const table = fs.readFileSync(url)
-const ogTable = fs.readFileSync(ogUrl)
-
-export const nMembers = () => {
-  const $ = load(table)
-  const items = $('tbody tr').toArray().map((member) => {
-    const content = load(member)
-
-    return {
-      name: content('td:nth-child(1)').text(),
-      kana: content('td:nth-child(2)').text(),
-      cate: content('td:nth-child(7)').text(),
-      birthday: content('td:nth-child(3)').text().split('_')[0],
-      birthplace: content('td:nth-child(4)').text(),
-      blood: content('td:nth-child(5)').text(),
-      height: content('td:nth-child(6)').text().split('&')[0],
-    }
-  }).filter(Boolean)
-
-  const og = load(ogTable)
-  const ogItems = og('tbody tr').toArray().map((member) => {
-    const content  = load(member)
-
-    return {
-      name: content('td:nth-child(1)').text(),
-      kana: content('td:nth-child(2)').text(),
-      cate: content('td:nth-child(7)').text(),
-      birthday: content('td:nth-child(3)').text().split('_')[0],
-      birthplace: content('td:nth-child(4)').text(),
-      blood: content('td:nth-child(5)').text().trim(),
-      height: content('td:nth-child(6)').text().trim().split('&')[0],
-    }
-  }).reverse()
-
-  return items.concat(ogItems)
-}
-
-export const getCacheNoGiMembers = cachedFunction(async () => {
-  // TODO: get wiki data
-  const data = nMembers()
-
-  return data
-}, {
-  maxAge: 60 * 60 * 12 * 7, // one week cached
-  name: 'noMemberInfo'
-})
