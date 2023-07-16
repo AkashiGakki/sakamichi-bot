a15a00023e18ffac4c25156f6405cb4375152954
Akashi <akashi_sai@163.com>
2023-07-16 09:06:12 +0800

    :zip: chore: nogi data

--- group/nogi/fifth.ts
+++ group/nogi/fifth.ts
@@ -7,49 +7,49 @@ export const fifthOsu = [
 ]
 
 export const fifthMember = [{
-  ...fifth['inoue nagi'],
-  age: getAge(fifth['inoue nagi'].birth),
-  constellation: getConstellation(fifth['inoue nagi'].birth, 'ja'),
+  ...fifth['inoue_nagi'],
+  age: getAge(fifth['inoue_nagi'].birth),
+  constellation: getConstellation(fifth['inoue_nagi'].birth, 'ja'),
 }, {
-  ...fifth['ikeda teresa'],
-  age: getAge(fifth['ikeda teresa'].birth),
+  ...fifth['ikeda_teresa'],
+  age: getAge(fifth['ikeda_teresa'].birth),
   constellation: getConstellation(fifth['ikeda teresa'].birth, 'ja'),
 }, {
-  ...fifth["sugawara satsuki"],
-  age: getAge(fifth['sugawara satsuki'].birth),
-  constellation: getConstellation(fifth['sugawara satsuki'].birth, 'ja'),
+  ...fifth["sugawara_satsuki"],
+  age: getAge(fifth['sugawara_satsuki'].birth),
+  constellation: getConstellation(fifth['sugawara_satsuki'].birth, 'ja'),
 }, {
-  ...fifth['kawasaki sakura'],
-  age: getAge(fifth['kawasaki sakura'].birth),
-  constellation: getConstellation(fifth['kawasaki sakura'].birth, 'ja'),
+  ...fifth['kawasaki_sakura'],
+  age: getAge(fifth['kawasaki_sakura'].birth),
+  constellation: getConstellation(fifth['kawasaki_sakura'].birth, 'ja'),
 }, {
-  ...fifth['nakanishi aruno'],
-  age: getAge(fifth['nakanishi aruno'].birth),
-  constellation: getConstellation(fifth['nakanishi aruno'].birth, 'ja'),
+  ...fifth['nakanishi_aruno'],
+  age: getAge(fifth['nakanishi_aruno'].birth),
+  constellation: getConstellation(fifth['nakanishi_aruno'].birth, 'ja'),
 }, {
-  ...fifth['tomisato nao'],
-  age: getAge(fifth['tomisato nao'].birth),
-  constellation: getConstellation(fifth['tomisato nao'].birth, 'ja'),
+  ...fifth['tomisato_nao'],
+  age: getAge(fifth['tomisato_nao'].birth),
+  constellation: getConstellation(fifth['tomisato_nao'].birth, 'ja'),
 }, {
-  ...fifth['ichinose miku'],
-  age: getAge(fifth['ichinose miku'].birth),
-  constellation: getConstellation(fifth['ichinose miku'].birth, 'ja'),
+  ...fifth['ichinose_miku'],
+  age: getAge(fifth['ichinose_miku'].birth),
+  constellation: getConstellation(fifth['ichinose_miku'].birth, 'ja'),
 }, {
-  ...fifth['ioki mao'],
-  age: getAge(fifth['ioki mao'].birth),
-  constellation: getConstellation(fifth['ioki mao'].birth, 'ja'),
+  ...fifth['ioki_mao'],
+  age: getAge(fifth['ioki_mao'].birth),
+  constellation: getConstellation(fifth['ioki_mao'].birth, 'ja'),
 }, {
-  ...fifth['okuda iroha'],
-  age: getAge(fifth['okuda iroha'].birth),
-  constellation: getConstellation(fifth['okuda iroha'].birth, 'ja'),
+  ...fifth['okuda_iroha'],
+  age: getAge(fifth['okuda_iroha'].birth),
+  constellation: getConstellation(fifth['okuda_iroha'].birth, 'ja'),
 }, {
-  ...fifth['ogawa aya'],
-  age: getAge(fifth['ogawa aya'].birth),
-  constellation: getConstellation(fifth['ogawa aya'].birth, 'ja'),
+  ...fifth['ogawa_aya'],
+  age: getAge(fifth['ogawa_aya'].birth),
+  constellation: getConstellation(fifth['ogawa_aya'].birth, 'ja'),
 }, {
-  ...fifth['okamoto hina'],
-  age: getAge(fifth['okamoto hina'].birth),
-  constellation: getConstellation(fifth['okamoto hina'].birth, 'ja'),
+  ...fifth['okamoto_hina'],
+  age: getAge(fifth['okamoto_hina'].birth),
+  constellation: getConstellation(fifth['okamoto_hina'].birth, 'ja'),
 }]
 
 export const fifthGraduated = []
--- group/nogi/first.ts
+++ group/nogi/first.ts
@@ -11,137 +11,137 @@ export const firstOsu = [
 export const firstMember = []
 
 export const firstGraduated = [{
-  ...first['saito asuka'],
-  age: getAge(first['saito asuka'].birth),
-  constellation: getConstellation(first['saito asuka'].birth, 'ja')
+  ...first['saito_asuka'],
+  age: getAge(first['saito_asuka'].birth),
+  constellation: getConstellation(first['saito_asuka'].birth, 'ja')
 }, {
-  ...first['nishino nanase'],
-  age: getAge(first['nishino nanase'].birth),
-  constellation: getConstellation(first['nishino nanase'].birth, 'ja')
+  ...first['nishino_nanase'],
+  age: getAge(first['nishino_nanase'].birth),
+  constellation: getConstellation(first['nishino_nanase'].birth, 'ja')
 }, {
-  ...first['hashimoto nanami'],
-  age: getAge(first['hashimoto nanami'].birth),
-  constellation: getConstellation(first['hashimoto nanami'].birth, 'ja')
+  ...first['hashimoto_nanami'],
+  age: getAge(first['hashimoto_nanami'].birth),
+  constellation: getConstellation(first['hashimoto_nanami'].birth, 'ja')
 }, {
-  ...first['shiraishi mai'],
-  age: getAge(first['shiraishi mai'].birth),
-  constellation: getConstellation(first['shiraishi mai'].birth, 'ja')
+  ...first['shiraishi_mai'],
+  age: getAge(first['shiraishi_mai'].birth),
+  constellation: getConstellation(first['shiraishi_mai'].birth, 'ja')
 }, {
-  ...first['ikuta erika'],
-  age: getAge(first['ikuta erika'].birth),
-  constellation: getConstellation(first['ikuta erika'].birth, 'ja')
+  ...first['ikuta_erika'],
+  age: getAge(first['ikuta_erika'].birth),
+  constellation: getConstellation(first['ikuta_erika'].birth, 'ja')
 }, {
-  ...first['itou marika'],
-  age: getAge(first['itou marika'].birth),
-  constellation: getConstellation(first['itou marika'].birth, 'ja')
+  ...first['itou_marika'],
+  age: getAge(first['itou_marika'].birth),
+  constellation: getConstellation(first['itou_marika'].birth, 'ja')
 }, {
-  ...first['hoshino minami'],
-  age: getAge(first['hoshino minami'].birth),
-  constellation: getConstellation(first['hoshino minami'].birth, 'ja')
+  ...first['hoshino_minami'],
+  age: getAge(first['hoshino_minami'].birth),
+  constellation: getConstellation(first['hoshino_minami'].birth, 'ja')
 }, {
-  ...first['etou misa'],
-  age: getAge(first['etou misa'].birth),
-  constellation: getConstellation(first['etou misa'].birth, 'ja')
+  ...first['etou_misa'],
+  age: getAge(first['etou_misa'].birth),
+  constellation: getConstellation(first['etou_misa'].birth, 'ja')
 }, {
-  ...first['akimoto manatsu'],
-  age: getAge(first['akimoto manatsu'].birth),
-  constellation: getConstellation(first['akimoto manatsu'].birth, 'ja')
+  ...first['akimoto_manatsu'],
+  age: getAge(first['akimoto_manatsu'].birth),
+  constellation: getConstellation(first['akimoto_manatsu'].birth, 'ja')
 }, {
-  ...first['matsumura sayuri'],
-  age: getAge(first['matsumura sayuri'].birth),
-  constellation: getConstellation(first['matsumura sayuri'].birth, 'ja')
+  ...first['matsumura_sayuri'],
+  age: getAge(first['matsumura_sayuri'].birth),
+  constellation: getConstellation(first['matsumura_sayuri'].birth, 'ja')
 }, {
-  ...first['takayama kazumi'],
-  age: getAge(first['takayama kazumi'].birth),
-  constellation: getConstellation(first['takayama kazumi'].birth, 'ja')
+  ...first['takayama_kazumi'],
+  age: getAge(first['takayama_kazumi'].birth),
+  constellation: getConstellation(first['takayama_kazumi'].birth, 'ja')
 }, {
-  ...first['sakurai reika'],
-  age: getAge(first['sakurai reika'].birth),
-  constellation: getConstellation(first['sakurai reika'].birth, 'ja')
+  ...first['sakurai_reika'],
+  age: getAge(first['sakurai_reika'].birth),
+  constellation: getConstellation(first['sakurai_reika'].birth, 'ja')
 }, {
-  ...first['nakamoto himeka'],
-  age: getAge(first['nakamoto himeka'].birth),
-  constellation: getConstellation(first['nakamoto himeka'].birth, 'ja')
+  ...first['nakamoto_himeka'],
+  age: getAge(first['nakamoto_himeka'].birth),
+  constellation: getConstellation(first['nakamoto_himeka'].birth, 'ja')
 }, {
-  ...first['fukagawa mai'],
-  age: getAge(first['fukagawa mai'].birth),
-  constellation: getConstellation(first['fukagawa mai'].birth, 'ja')
+  ...first['fukagawa_mai'],
+  age: getAge(first['fukagawa_mai'].birth),
+  constellation: getConstellation(first['fukagawa_mai'].birth, 'ja')
 }, {
-  ...first['inoue sayuri'],
-  age: getAge(first['inoue sayuri'].birth),
-  constellation: getConstellation(first['inoue sayuri'].birth, 'ja')
+  ...first['inoue_sayuri'],
+  age: getAge(first['inoue_sayuri'].birth),
+  constellation: getConstellation(first['inoue_sayuri'].birth, 'ja')
 }, {
-  ...first['saitou yuuri'],
-  age: getAge(first['saitou yuuri'].birth),
-  constellation: getConstellation(first['saitou yuuri'].birth, 'ja')
+  ...first['saitou_yuuri'],
+  age: getAge(first['saitou_yuuri'].birth),
+  constellation: getConstellation(first['saitou_yuuri'].birth, 'ja')
 }, {
-  ...first['ikoma rina'],
-  age: getAge(first['ikoma rina'].birth),
-  constellation: getConstellation(first['ikoma rina'].birth, 'ja')
+  ...first['ikoma_rina'],
+  age: getAge(first['ikoma_rina'].birth),
+  constellation: getConstellation(first['ikoma_rina'].birth, 'ja')
 }, {
-  ...first['noujou ami'],
-  age: getAge(first['noujou ami'].birth),
-  constellation: getConstellation(first['noujou ami'].birth, 'ja')
+  ...first['noujou_ami'],
+  age: getAge(first['noujou_ami'].birth),
+  constellation: getConstellation(first['noujou_ami'].birth, 'ja')
 }, {
-  ...first['wakatsuki yumi'],
-  age: getAge(first['wakatsuki yumi'].birth),
-  constellation: getConstellation(first['wakatsuki yumi'].birth, 'ja')
+  ...first['wakatsuki_yumi'],
+  age: getAge(first['wakatsuki_yumi'].birth),
+  constellation: getConstellation(first['wakatsuki_yumi'].birth, 'ja')
 }, {
-  ...first['kawamura mahiro'],
-  age: getAge(first['kawamura mahiro'].birth),
-  constellation: getConstellation(first['kawamura mahiro'].birth, 'ja')
+  ...first['kawamura_mahiro'],
+  age: getAge(first['kawamura_mahiro'].birth),
+  constellation: getConstellation(first['kawamura_mahiro'].birth, 'ja')
 }, {
-  ...first['kawago hina'],
-  age: getAge(first['kawago hina'].birth),
-  constellation: getConstellation(first['kawago hina'].birth, 'ja')
+  ...first['kawago_hina'],
+  age: getAge(first['kawago_hina'].birth),
+  constellation: getConstellation(first['kawago_hina'].birth, 'ja')
 }, {
-  ...first['higuchi hina'],
-  age: getAge(first['higuchi hina'].birth),
-  constellation: getConstellation(first['higuchi hina'].birth, 'ja')
+  ...first['higuchi_hina'],
+  age: getAge(first['higuchi_hina'].birth),
+  constellation: getConstellation(first['higuchi_hina'].birth, 'ja')
 }, {
-  ...first['wada maaya'],
-  age: getAge(first['wada maaya'].birth),
-  constellation: getConstellation(first['wada maaya'].birth, 'ja')
+  ...first['wada_maaya'],
+  age: getAge(first['wada_maaya'].birth),
+  constellation: getConstellation(first['wada_maaya'].birth, 'ja')
 }, {
-  ...first['nagashima seira'],
-  age: getAge(first['nagashima seira'].birth),
-  constellation: getConstellation(first['nagashima seira'].birth, 'ja')
+  ...first['nagashima_seira'],
+  age: getAge(first['nagashima_seira'].birth),
+  constellation: getConstellation(first['nagashima_seira'].birth, 'ja')
 }, {
-  ...first['saitou chiharu'],
-  age: getAge(first['saitou chiharu'].birth),
-  constellation: getConstellation(first['saitou chiharu'].birth, 'ja')
+  ...first['saitou_chiharu'],
+  age: getAge(first['saitou_chiharu'].birth),
+  constellation: getConstellation(first['saitou_chiharu'].birth, 'ja')
 }, {
-  ...first['nakada kana'],
-  age: getAge(first['nakada kana'].birth),
-  constellation: getConstellation(first['nakada kana'].birth, 'ja')
+  ...first['nakada_kana'],
+  age: getAge(first['nakada_kana'].birth),
+  constellation: getConstellation(first['nakada_kana'].birth, 'ja')
 }, {
-  ...first['itou nene'],
-  age: getAge(first['itou nene'].birth),
-  constellation: getConstellation(first['itou nene'].birth, 'ja')
+  ...first['itou_nene'],
+  age: getAge(first['itou_nene'].birth),
+  constellation: getConstellation(first['itou_nene'].birth, 'ja')
 }, {
-  ...first['ichiki rena'],
-  age: getAge(first['ichiki rena'].birth),
-  constellation: getConstellation(first['ichiki rena'].birth, 'ja')
+  ...first['ichiki_rena'],
+  age: getAge(first['ichiki_rena'].birth),
+  constellation: getConstellation(first['ichiki_rena'].birth, 'ja')
 }, {
-  ...first['kashiwa yukina'],
-  age: getAge(first['kashiwa yukina'].birth),
-  constellation: getConstellation(first['kashiwa yukina'].birth, 'ja')
+  ...first['kashiwa_yukina'],
+  age: getAge(first['kashiwa_yukina'].birth),
+  constellation: getConstellation(first['kashiwa_yukina'].birth, 'ja')
 }, {
-  ...first['iwase yumiko'],
-  age: getAge(first['iwase yumiko'].birth),
-  constellation: getConstellation(first['iwase yumiko'].birth, 'ja')
+  ...first['iwase_yumiko'],
+  age: getAge(first['iwase_yumiko'].birth),
+  constellation: getConstellation(first['iwase_yumiko'].birth, 'ja')
 }, {
-  ...first['hatanaka seira'],
-  age: getAge(first['hatanaka seira'].birth),
-  constellation: getConstellation(first['hatanaka seira'].birth, 'ja')
+  ...first['hatanaka_seira'],
+  age: getAge(first['hatanaka_seira'].birth),
+  constellation: getConstellation(first['hatanaka_seira'].birth, 'ja')
 }, {
-  ...first['miyazawa seira'],
-  age: getAge(first['miyazawa seira'].birth),
-  constellation: getConstellation(first['miyazawa seira'].birth, 'ja')
+  ...first['miyazawa_seira'],
+  age: getAge(first['miyazawa_seira'].birth),
+  constellation: getConstellation(first['miyazawa_seira'].birth, 'ja')
 }, {
-  ...first['yamato rina'],
-  age: getAge(first['yamato rina'].birth),
-  constellation: getConstellation(first['yamato rina'].birth, 'ja')
+  ...first['yamato_rina'],
+  age: getAge(first['yamato_rina'].birth),
+  constellation: getConstellation(first['yamato_rina'].birth, 'ja')
 }]
 
 export const cateFirst = firstMember.concat(firstGraduated)
