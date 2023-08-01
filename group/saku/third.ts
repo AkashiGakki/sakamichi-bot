abf0b039bd1a2f8048dde013ba62ef62193e348a
Akashi <akashi_sai@163.com>
2023-08-01 23:44:46 +0800

    :zap: feat(saku): add field

--- group/saku/third.ts
+++ group/saku/third.ts
@@ -1,1 +1,16 @@
-export const cateFirst = [{}]
+import { getAge, getConstellation } from '@use-kit/functions'
+import third from './json/third.json'
+
+export const thirdMember = [{
+  ...third['odakura_reina'],
+  age: getAge(third['odakura_reina'].birth),
+  constellation: getConstellation(third['odakura_reina'].birth, 'ja')
+}, {
+  ...third['murayama_miu'],
+  age: getAge(third['murayama_miu'].birth),
+  constellation: getConstellation(third['murayama_miu'].birth, 'ja')
+}]
+
+export const thirdGraduated = []
+
+export const cateThird = thirdMember.concat(thirdGraduated)
